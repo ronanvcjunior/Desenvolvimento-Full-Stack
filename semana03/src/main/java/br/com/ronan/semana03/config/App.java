2cc65b379aba710d808d9001e36af7742d590c5f
Ronan Vieira do Carmo Junior <ronanvcjunior@gmail.com>
2021-08-16 23:59:20 -0300

    semana03

--- semana03/src/main/java/br/com/ronan/semana03/config/App.java
+++ semana03/src/main/java/br/com/ronan/semana03/config/App.java
@@ -1,5 +1,8 @@
 package br.com.ronan.semana03.config;
 
+import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.List;
 
 import javax.persistence.EntityManager;
@@ -13,118 +16,185 @@ import br.com.ronan.semana03.entities.Hospedagem;
 import br.com.ronan.semana03.entities.Hospede;
 import br.com.ronan.semana03.entities.Hotel;
 import br.com.ronan.semana03.entities.Quarto;
+import br.com.ronan.semana03.enums.CategoriaQuarto;
 
 public class App {
     public static void main( String[] args ) {
-        testarHospedagemDao();
+        testarConexaoComBancoDeDados();
     }
 
-    public static void testarHospedagemDao() {
-        System.out.println("Inicio");
+    // public static void testarHospedagemDao() {
+    //     System.out.println("Inicio");
 
-        HospedagemCtrl hospedagemCtrl = new HospedagemCtrl();
+    //     HospedagemCtrl hospedagemCtrl = new HospedagemCtrl();
 
-        testarHospedagemFindAll(hospedagemCtrl);
+    //     // testarHospedagemFindAll(hospedagemCtrl);
 
-        // testarHospedagemFindById(hospedagemCtrl, 2);
+    //     // testarHospedagemFindById(hospedagemCtrl, 1);
 
-        // Hospedagem h = new Hospedagem("Castle Host", "Inglaterra", 5);
-        // testarHospedagemInserir(hospedagemCtrl, h);
+    //     // QuartoCtrl quartoCtrl = new QuartoCtrl();
+    //     // Quarto quarto = quartoCtrl.findById(6);
+    //     // HospedeCtrl hospedeCtrl = new HospedeCtrl();
+    //     // Hospede hospede = hospedeCtrl.findById(3);
+    //     // Date dtCheckin = converterStirngEmDateSql("22/08/2021");
+    //     // Date dtCheckout = converterStirngEmDateSql("05/09/2021");
+    //     // Hospedagem h = new Hospedagem(quarto, hospede, dtCheckin, dtCheckout);
+    //     // testarHospedagemInserir(hospedagemCtrl, h);
 
-        // Hospedagem h = hospedagemCtrl.findById(19);
-        // h.setNmHospedagem("DigiHotel Master");
-        // h.setEndereco("DigiWorld");
-        // h.setQtdEstrelas(5);
-        // testarHospedagemUpdate(hospedagemCtrl, h);
+    //     // Hospedagem h = hospedagemCtrl.findById(3);
+    //     // Date dtCheckout = converterStirngEmDateSql("12/09/2021");
+    //     // h.setDtCheckout(dtCheckout);
+    //     // testarHospedagemUpdate(hospedagemCtrl, h);
 
-        // testarHospeagemeDelete(hospedagemCtrl, 15);
+    //     testarHospedagemDelete(hospedagemCtrl, 4);
 
-        System.out.println("Fim");
-    }
+    //     System.out.println("Fim");
+    // }
 
-    public static void testarHospedagemFindAll(HospedagemCtrl hospedagemCtrl) {
-        List<Hospedagem> hospedagens = hospedagemCtrl.findAll();
+    // public static void testarHospedagemFindAll(HospedagemCtrl hospedagemCtrl) {
+    //     List<Hospedagem> hospedagens = hospedagemCtrl.findAll();
 
-        for (Hospedagem hospedagem : hospedagens) {
-            System.out.println(hospedagem.toString());
-        }
-    }
+    //     for (Hospedagem hospedagem : hospedagens) {
+    //         System.out.println(hospedagem.toString());
+    //     }
+    // }
 
+    // public static void testarHospedagemFindById(HospedagemCtrl hospedagemCtrl, Integer id) {
+    //     Hospedagem hospedagem = hospedagemCtrl.findById(id);
 
+    //     System.out.println(hospedagem.toString());
+    // }
 
+    // public static void testarHospedagemInserir(HospedagemCtrl hospedagemCtrl, Hospedagem h) {
+    //     Hospedagem hospedagem = hospedagemCtrl.insert(h);
 
+    //     System.out.println(hospedagem.toString());
+    // }
 
+    // public static void testarHospedagemUpdate(HospedagemCtrl hospedagemCtrl, Hospedagem h) {
+    //     Hospedagem hospedagem = hospedagemCtrl.update(h);
 
+    //     System.out.println(hospedagem.toString());
+    // }
 
-    public static void testarHospedeDao() {
-        System.out.println("Inicio");
-
-        HospedeCtrl hospedeCtrl = new HospedeCtrl();
-
-        testarHospedeFindAll(hospedeCtrl);
-
-        // testarHospedeFindById(hospedeCtrl, 2);
-
-        // Hospede h = new Hospede("Castle Host", "Inglaterra", 5);
-        // testarHospedeInserir(hospedeCtrl, h);
-
-        // Hospede h = hospedeCtrl.findById(19);
-        // h.setNmHospede("DigiHotel Master");
-        // h.setEndereco("DigiWorld");
-        // h.setQtdEstrelas(5);
-        // testarHospedeUpdate(hospedeCtrl, h);
-
-        // testarHospedeDelete(hospedeCtrl, 15);
-
-        System.out.println("Fim");
-    }
-
-    public static void testarHospedeFindAll(HospedeCtrl hospedeCtrl) {
-        List<Hospede> hospedes = hospedeCtrl.findAll();
-
-        for (Hospede hospede : hospedes) {
-            System.out.println(hospede.toString());
-        }
-    }
+    // public static void testarHospedagemDelete(HospedagemCtrl hospedagemCtrl, Integer id) {
+    //     hospedagemCtrl.delete(id);
+    // }
 
 
 
 
 
 
+    // public static void testarHospedeDao() {
+    //     System.out.println("Inicio");
 
-    public static void testarQuartoDao() {
-        System.out.println("Inicio");
+    //     HospedeCtrl hospedeCtrl = new HospedeCtrl();
 
-        QuartoCtrl quartoCtrl = new QuartoCtrl();
+    //     // testarHospedeFindAll(hospedeCtrl);
 
-        testarQuartoFindAll(quartoCtrl);
+    //     // testarHospedeFindById(hospedeCtrl, 2);
 
-        // testarQuartoFindById(quartoCtrl, 2);
+    //     // Date dtNascimento = converterStirngEmDateSql("05/05/2002");
+    //     // Hospede h = new Hospede("Luffy", dtNascimento, 5);
+    //     // testarHospedeInserir(hospedeCtrl, h);
 
-        // Quarto h = new Quarto("Castle Host", "Inglaterra", 5);
-        // testarQuartoInserir(QuartoCtrl, h);
+    //     // Hospede h = hospedeCtrl.findById(1);
+    //     // h.setNmHospede("Nami");
+    //     // Date dtNascimento = converterStirngEmDateSql("03/07/2001");
+    //     // h.setDtNascimento(dtNascimento);
+    //     // testarHospedeUpdate(hospedeCtrl, h);
 
-        // Quarto h = quartoCtrl.findById(19);
-        // h.setNmQuarto("DigiHotel Master");
-        // h.setEndereco("DigiWorld");
-        // h.setQtdEstrelas(5);
-        // testarQuartoUpdate(quartoCtrl, h);
+    //     // testarHospedeDelete(hospedeCtrl, 2);
 
-        //testarQuartoDelete(quartoCtrl, 15);
-        
-        System.out.println("Fim");
-    }
+    //     System.out.println("Fim");
+    // }
 
-    public static void testarQuartoFindAll(QuartoCtrl quartoCtrl) {
-        List<Quarto> quartos = quartoCtrl.findAll();
+    // public static void testarHospedeFindAll(HospedeCtrl hospedeCtrl) {
+    //     List<Hospede> hospedes = hospedeCtrl.findAll();
 
-        for (Quarto quarto : quartos) {
-        System.out.println(quarto.toString());
-        }
-    }
+    //     for (Hospede hospede : hospedes) {
+    //         System.out.println(hospede.toString());
+    //     }
+    // }
+
+    // public static void testarHospedeFindById(HospedeCtrl hospedeCtrl, Integer id) {
+    //     Hospede hospede = hospedeCtrl.findById(id);
+
+    //     System.out.println(hospede.toString());
+    // }
+
+    // public static void testarHospedeInserir(HospedeCtrl hospedeCtrl, Hospede h) {
+    //     Hospede hospede = hospedeCtrl.insert(h);
+
+    //     System.out.println(hospede.toString());
+    // }
+
+    // public static void testarHospedeUpdate(HospedeCtrl hospedeCtrl, Hospede h) {
+    //     Hospede hospede = hospedeCtrl.update(h);
+
+    //     System.out.println(hospede.toString());
+    // }
+
+    // public static void testarHospedeDelete(HospedeCtrl hospedeCtrl, Integer id) {
+    //     hospedeCtrl.delete(id);
+    // }
 
 
+
+
+
+
+    // public static void testarQuartoDao() {
+    //     System.out.println("Inicio");
+
+    //     QuartoCtrl quartoCtrl = new QuartoCtrl();
+
+    //     // testarQuartoFindAll(quartoCtrl);
+
+    //     // testarQuartoFindById(quartoCtrl, 1);
+
+    //     // HotelCtrl hotelCtrl = new HotelCtrl();
+    //     // Hotel hotel = hotelCtrl.findById(2);
+    //     // Quarto q = new Quarto(hotel, CategoriaQuarto.LUXO, 2, 2, 250.);
+    //     // testarQuartoInserir(quartoCtrl, q);
+
+    //     // Quarto q = quartoCtrl.findById(2);
+    //     // q.setNrQuarto(5);
+    //     // testarQuartoUpdate(quartoCtrl, q);
+
+    //     // testarQuartoDelete(quartoCtrl, 7);
+    // }
+
+    // public static void testarQuartoFindAll(QuartoCtrl quartoCtrl) {
+    //     List<Quarto> quartos = quartoCtrl.findAll();
+
+    //     for (Quarto quarto : quartos) {
+    //     System.out.println(quarto.toString());
+    //     }
+    // }
+
+    // public static void testarQuartoFindById(QuartoCtrl quartoCtrl, Integer id) {
+    //     Quarto quarto = quartoCtrl.findById(id);
+
+    //     System.out.println(quarto.toString());
+    // }
+
+    // public static void testarQuartoInserir(QuartoCtrl quartoCtrl, Quarto q) {
+    //     Quarto quarto = quartoCtrl.insert(q);
+
+    //     System.out.println(quarto.toString());
+    // }
+
+    // public static void testarQuartoUpdate(QuartoCtrl quartoCtrl, Quarto h) {
+    //     Quarto quarto = quartoCtrl.update(h);
+
+    //     System.out.println(quarto.toString());
+    // }
+
+    // public static void testarQuartoDelete(QuartoCtrl quartoCtrl, Integer id) {
+    //     quartoCtrl.delete(id);
+    // }
 
 
     
@@ -187,84 +257,101 @@ public class App {
 
 
 
-    // public static void testarConexaoComBancoDeDados() {
-    //     System.out.println("Inicio");
+    public static void testarConexaoComBancoDeDados() {
+        System.out.println("Inicio");
 
-    //     EntityManager em = DaoFactory.getEm();
+        EntityManager em = DaoFactory.getEm();
 
-    //     //testarInserirHotel(em);
-    //     //testarPesquisarAllHotel(em);
-    //     //testarPesquisarPorIdHotel(em, 14);
-    //     //testarPesquisarPorCampoNomeHotel(em, "as");
-    //     //testarPesquisarPorCampoQtdEstrelasHotel(em, 2);
-    //     //testarEditarHotel(em, 16);
-    //     //testarDeletarHotel(em, 14);
+        //testarInserirHotel(em);
+        testarPesquisarAllHotel(em);
+        //testarPesquisarPorIdHotel(em, 14);
+        //testarPesquisarPorCampoNomeHotel(em, "as");
+        //testarPesquisarPorCampoQtdEstrelasHotel(em, 2);
+        //testarEditarHotel(em, 16);
+        //testarDeletarHotel(em, 14);
 
-    //     System.out.println("Fim");
-    // }
+        System.out.println("Fim");
+    }
 
-    // public static void testarInserirHotel(EntityManager em) {
-    //     Hotel hotel = new Hotel("Denali Hotel", "Anápolis", 4);
+    public static void testarInserirHotel(EntityManager em) {
+        Hotel hotel = new Hotel("Denali Hotel", "Anápolis", 4);
 
-    //     em.getTransaction().begin();
-    //     em.persist(hotel);
-    //     em.getTransaction().commit();
-    // }
+        em.getTransaction().begin();
+        em.persist(hotel);
+        em.getTransaction().commit();
+    }
 
-    // public static void testarPesquisarAllHotel(EntityManager em) {
-    //     TypedQuery<Hotel> result = em.createQuery("FROM Hotel", Hotel.class);
+    public static void testarPesquisarAllHotel(EntityManager em) {
+        TypedQuery<Hotel> result = em.createQuery("FROM Hotel", Hotel.class);
 
-    //     List<Hotel> hoteis = result.getResultList();
+        List<Hotel> hoteis = result.getResultList();
         
-    //     for (Hotel hotel : hoteis) {
-    //         System.out.println(hotel.toString());
-    //     }
-    // }
+        for (Hotel hotel : hoteis) {
+            System.out.println(hotel.toString());
+        }
+    }
 
-    // public static void testarPesquisarPorIdHotel(EntityManager em, Integer id) {
-    //     Hotel hotel = em.find(Hotel.class, id);
+    public static void testarPesquisarPorIdHotel(EntityManager em, Integer id) {
+        Hotel hotel = em.find(Hotel.class, id);
 
-    //     System.out.println(hotel.toString());
-    // }
+        System.out.println(hotel.toString());
+    }
 
-    // public static void testarPesquisarPorCampoNomeHotel(EntityManager em, String str) {
-    //     String sql = "FROM Hotel h WHERE h.nmHotel LIKE :str";
-    //     TypedQuery<Hotel> result = em.createQuery(sql, Hotel.class);
-    //     result.setParameter("str", "%"+str+"%");
+    public static void testarPesquisarPorCampoNomeHotel(EntityManager em, String str) {
+        String sql = "FROM Hotel h WHERE h.nmHotel LIKE :str";
+        TypedQuery<Hotel> result = em.createQuery(sql, Hotel.class);
+        result.setParameter("str", "%"+str+"%");
 
-    //     List<Hotel> hoteis = result.getResultList();
+        List<Hotel> hoteis = result.getResultList();
 
-    //     for (Hotel hotel : hoteis) {
-    //         System.out.println(hotel.toString());
-    //     }
-    // }
+        for (Hotel hotel : hoteis) {
+            System.out.println(hotel.toString());
+        }
+    }
 
-    // public static void testarPesquisarPorCampoQtdEstrelasHotel(EntityManager em, Integer str) {
-    //     String sql = "FROM Hotel h WHERE h.qtdEstrelas LIKE :str";
-    //     TypedQuery<Hotel> result = em.createQuery(sql, Hotel.class);
-    //     result.setParameter("str", str);
+    public static void testarPesquisarPorCampoQtdEstrelasHotel(EntityManager em, Integer str) {
+        String sql = "FROM Hotel h WHERE h.qtdEstrelas LIKE :str";
+        TypedQuery<Hotel> result = em.createQuery(sql, Hotel.class);
+        result.setParameter("str", str);
 
-    //     List<Hotel> hoteis = result.getResultList();
+        List<Hotel> hoteis = result.getResultList();
 
-    //     for (Hotel hotel : hoteis) {
-    //         System.out.println(hotel.toString());
-    //     }
-    // }
+        for (Hotel hotel : hoteis) {
+            System.out.println(hotel.toString());
+        }
+    }
 
-    // public static void testarEditarHotel(EntityManager em, Integer id) {
-    //     Hotel hotel = em.find(Hotel.class, id);
-    //     hotel.setNmHotel("Castor");
+    public static void testarEditarHotel(EntityManager em, Integer id) {
+        Hotel hotel = em.find(Hotel.class, id);
+        hotel.setNmHotel("Castor");
 
-    //     em.getTransaction().begin();
-    //     em.persist(hotel);
-    //     em.getTransaction().commit();
-    // }
+        em.getTransaction().begin();
+        em.persist(hotel);
+        em.getTransaction().commit();
+    }
 
-    // public static void testarDeletarHotel(EntityManager em, Integer id) {
-    //     Hotel hotel = em.find(Hotel.class, id);
+    public static void testarDeletarHotel(EntityManager em, Integer id) {
+        Hotel hotel = em.find(Hotel.class, id);
 
-    //     em.getTransaction().begin();
-    //     em.remove(hotel);
-    //     em.getTransaction().commit();
-    // }
+        em.getTransaction().begin();
+        em.remove(hotel);
+        em.getTransaction().commit();
+    }
+
+    public static java.sql.Date converterStirngEmDateSql(String dateStr) {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+        java.sql.Date dateSql = null;
+        try {
+            java.util.Date dateSDF = sdf.parse(dateStr);
+            long ms = dateSDF.getTime();
+            dateSql = new java.sql.Date(ms);
+
+            return dateSql;
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
 }
